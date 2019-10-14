4470ba9ffe6728070d1325fa02cb71c09bab5ab4
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2019-10-13 21:41:06 -0400

    Add avg error calculations

--- LINQAgain/Program.cs
+++ LINQAgain/Program.cs
@@ -4,21 +4,26 @@ using PINQ;
 
 namespace LINQAgain {
     class Program {
+
+        // Obtaining the data source 
+        static MedicalDataContext db = new MedicalDataContext();
+
+        // Create the query 
+        static IQueryable<BSOM_DataSet_revised> data = 
+            from c in db.BSOM_DataSet_reviseds
+            select c;
+
+        // Get PINQ object--just wraps data queryable in PINQ Queryable
+        static PINQueryable<BSOM_DataSet_revised> search =
+            new PINQueryable<BSOM_DataSet_revised>(data, new PINQAgent());
+
+        // Epsilon values we wish to test
+        static double[] epsilons = { 10, 5, 1, 0.5, 0.1, 0.01 };
+
+        // Number of iterations we wish to use for average calculations
+        static int numIters = 100;
+
         static void Main(string[] args) {
-            // Obtaining the data source 
-            var db = new MedicalDataContext();
-
-            // Create the query 
-            var query = from c in db.BSOM_DataSet_reviseds
-                        select c;
-            var data = query.AsQueryable();
-
-            // Get PINQ object--just wraps data queryable in PINQ Queryable
-            PINQueryable<BSOM_DataSet_revised> search =
-                new PINQueryable<BSOM_DataSet_revised>(data, new PINQAgent());
-
-            // Epsilon values we wish to test
-            double[] epsilons = { 10, 5, 1, 0.5, 0.1, 0.01 };
 
             // Count
             TestWholeCount(data, search, epsilons);
@@ -36,11 +41,55 @@ namespace LINQAgain {
             TestRangedCount(data, search, epsilons);
             Console.WriteLine();
 
-            GetErrorWholeCount(data, search, epsilons);
+            // Generate average error on count over numIter iterations
+            Console.WriteLine("Average error on counts...");
+            double[] avgWholeCountError = 
+                GenerateAverageErrors(GetErrorWholeCount);
+
+            for (int i = 0; i < avgWholeCountError.Length; i++) {
+                Console.WriteLine("Epsilon " + epsilons[i] + 
+                    " avg error: " + avgWholeCountError[i]);
+            }
+            Console.WriteLine();
+
+            // Generate average error on average over numIter iterations
+            Console.WriteLine("Average error on averages...");
+            double[] avgAverageError =
+                GenerateAverageErrors(GetErrorAverage);
+
+            for (int i = 0; i < avgWholeCountError.Length; i++) {
+                Console.WriteLine("Epsilon " + epsilons[i] +
+                    " avg error: " + avgAverageError[i]);
+            }
+            Console.WriteLine();
+
+            // Generate average error on sum over numIter iterations
+            Console.WriteLine("Average error on sums...");
+            double[] avgSumError =
+                GenerateAverageErrors(GetErrorAverage);
+
+            for (int i = 0; i < avgWholeCountError.Length; i++) {
+                Console.WriteLine("Epsilon " + epsilons[i] +
+                    " avg error: " + avgSumError[i]);
+            }
+            Console.WriteLine();
+
+            // Generate average error on sum over numIter iterations
+            Console.WriteLine("Average error on ranged counts...");
+            double[] avgRangeError =
+                GenerateAverageErrors(GetErrorRangedCount);
+
+            for (int i = 0; i < avgWholeCountError.Length; i++) {
+                Console.WriteLine("Epsilon " + epsilons[i] +
+                    " avg error: " + avgRangeError[i]);
+            }
+            Console.WriteLine();
 
             // Pause the application
             Console.ReadKey(true);
         }
+
+        #region Basic query tests
 
         static void TestWholeCount(IQueryable<BSOM_DataSet_revised> data, 
           PINQueryable<BSOM_DataSet_revised> search, double[] epsilons) {
@@ -51,23 +100,6 @@ namespace LINQAgain {
                     "Noisy Count (epsilon {0}): {1}", ep, 
                     search.NoisyCount(ep)));
             }
-        }
-
-        static double[] GetErrorWholeCount(IQueryable<BSOM_DataSet_revised> data,
-          PINQueryable<BSOM_DataSet_revised> search, double[] epsilons) {
-            // Get true value
-            int trueCount = data.Count();
-
-            // Get list to hold our answers
-            double[] result = new double[epsilons.Length];
-
-            // Calculate differences
-            short idx = 0;
-            foreach (double ep in epsilons) {
-                result[idx++] = trueCount - search.NoisyCount(ep);
-            }
-            
-            return result;
         }
 
         static void TestAverage(IQueryable<BSOM_DataSet_revised> data,
@@ -108,5 +140,103 @@ namespace LINQAgain {
                         x.O1_PI_01) < 0.8).NoisyCount(ep)));
             }
         }
+
+        #endregion
+
+        #region Average error calculations
+
+        static double[] GenerateAverageErrors(Func<
+          IQueryable<BSOM_DataSet_revised>, PINQueryable<BSOM_DataSet_revised>,
+          double[], double[]> inFunc) {
+            double[] avg = new double[epsilons.Length];
+            for (int i = 0; i < numIters; i++) {
+                double[] avgError = inFunc(data, search, epsilons);
+                for (int j = 0; j < epsilons.Length; j++) {
+                    avg[j] += avgError[j];
+                }
+            }
+
+            for (int i = 0; i < avg.Length; i++) {
+                avg[i] /= numIters;
+            }
+
+            return avg;
+        }
+
+        static double[] GetErrorWholeCount(IQueryable<BSOM_DataSet_revised> data,
+          PINQueryable<BSOM_DataSet_revised> search, double[] epsilons) {
+            // Get true value
+            int trueCount = data.Count();
+
+            // Get list to hold our answers
+            double[] result = new double[epsilons.Length];
+
+            // Calculate differences
+            short idx = 0;
+            foreach (double ep in epsilons) {
+                result[idx++] = trueCount - search.NoisyCount(ep);
+            }
+
+            return result;
+        }
+
+        static double[] GetErrorAverage(IQueryable<BSOM_DataSet_revised> data,
+          PINQueryable<BSOM_DataSet_revised> search, double[] epsilons) {
+            // Get true value
+            double trueAvg = data.Average(
+                x => Convert.ToDouble(x.O1_PI_01));
+
+            // Get list to hold our answers
+            double[] result = new double[epsilons.Length];
+
+            // Calculate differences
+            short idx = 0;
+            foreach (double ep in epsilons) {
+                result[idx++] = trueAvg - search.NoisyAverage(
+                    ep, x => Convert.ToDouble(x.O1_PI_01));
+            }
+
+            return result;
+        }
+
+        static double[] GetErrorSum(IQueryable<BSOM_DataSet_revised> data,
+          PINQueryable<BSOM_DataSet_revised> search, double[] epsilons) {
+            // Get true value
+            double trueSum = data.Sum(
+                x => Convert.ToDouble(x.O1_PI_01));
+
+            // Get list to hold our answers
+            double[] result = new double[epsilons.Length];
+
+            // Calculate differences
+            short idx = 0;
+            foreach (double ep in epsilons) {
+                result[idx++] = trueSum - search.NoisySum(
+                    ep, x => Convert.ToDouble(x.O1_PI_01));
+            }
+
+            return result;
+        }
+
+        static double[] GetErrorRangedCount(IQueryable<BSOM_DataSet_revised> data,
+          PINQueryable<BSOM_DataSet_revised> search, double[] epsilons) {
+            // Get true value
+            double trueSum = data.Where(
+                x => Convert.ToDouble(x.O1_PI_01) < 0.8).Count();
+
+            // Get list to hold our answers
+            double[] result = new double[epsilons.Length];
+
+            // Calculate differences
+            short idx = 0;
+            foreach (double ep in epsilons) {
+                result[idx++] = trueSum - search.Where(x => Convert.ToDouble(
+                    x.O1_PI_01) < 0.8).NoisyCount(ep);
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
